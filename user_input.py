017c268c4bd8b226ee1f6b891167550d09f825e5
demapis <debbienjenga@gmail.com>
2024-08-18 05:30:08 +0300

    ADD user_input.txt

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("what's your name? ")
+age = input("what's your age? ")
+location = input("what's your location? ")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
